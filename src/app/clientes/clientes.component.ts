93dc1eb4db28acb135685c8eb05a1e006fef6305
Jazmin Santiago <starfenix66@gmail.com>
2020-11-04 20:06:48 -0600

    LocalStorage - SetItem y GetItem

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes{
+  nombre: string;
+  apellido: string;
+  edad: number;
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-
+  clientes: Array<Clientes> = new Array<Clientes>();
   constructor() { }
 
   ngOnInit(): void {
+    /*this.clientes.push({
+      nombre: "Jazmín",
+      apellido: "Santiago",
+      edad: 23
+    },
+    {
+      nombre: "Saúl",
+      apellido: "Torres",
+      edad: 25
+    }
+    ) */
+  }
+
+  guardarCliente(){
+    localStorage.setItem("clientes", JSON.stringify(this.clientes))
+  }
+
+  leerCliente(){
+    this.clientes = JSON.parse(localStorage.getItem("clientes"))
   }
 
 }
