653ae0f16d5e5408fe2dd6f0bd67401f13b6b0a4
Jazmin Santiago <starfenix66@gmail.com>
2020-11-18 20:08:41 -0600

    Agregando buscador

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Clientes } from '../models/clientes';
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  cliente: Array<Clientes> = new Array<Clientes>();
+  constructor(public clienteServicio: ClientesService) { }
 
   ngOnInit(): void {
+    this.cliente = this.clienteServicio.clientesLocalStorage;
   }
+
+  buscarClientes(event){
+    let nombreBuscar: string = event.target.value;
+    this.cliente = this.clienteServicio.clientesLocalStorage.filter(x=>{
+      return x.nombre.toLocaleLowerCase().includes(nombreBuscar.toLocaleLowerCase())
+    })
+  }
+
 
 }
